b3221f3e23a7125342d335f6da143d450b0afc59
Helena Juliana <helenajuliana14@gmail.com>
2020-08-26 13:32:41 -0300

    Ajusta cadastrar

--- src/aplicacao_console/copy/Cadastrar.java
+++ src/aplicacao_console/copy/Cadastrar.java
@@ -1,6 +1,7 @@
 package aplicacao_console.copy;
 
 
+
 import fachada.Fachada;
 import modelo.Cliente;
 import modelo.Funcionario;
@@ -12,19 +13,21 @@ public class Cadastrar {
 	public Cadastrar(){
 		try {
 			Fachada.inicializar();
-		
+			Cliente cl;
+			Produto pro;
+			Funcionario fun;
 			
 			System.out.println("cadastrando...");
 			
-			//Fachada.cadastrarCliente(" Barto 2", "40001", "ruaendereco", "pl@gmail", "12312231");
+			//cl= Fachada.cadastrarCliente(" Barto", "4101001", "ruaendereco", "pl@gmail", "12312231");
 			
+			//System.out.println(cl);
 			
+//			pro= Fachada.cadastrarProdutos("22", "Vestido 2 ", 11, "Preto e branco  etc");
+//			System.out.println(pro);
 			
-			//Fachada.cadastrarProdutos("2", "Calça 2 ", 11, "Preto e branco  etc");
-			
-			
-			 //Fachada.cadastrarFuncionario("17115","Maria 1", "mariavendas@gmail.com");
-			
+			 fun= Fachada.cadastrarFuncionario("171115","Maria 1", "mariavendas@gmail.com");
+			 System.out.println(fun);
          	
 		} catch (Exception e) 	{
 			System.out.println(e.getMessage());
